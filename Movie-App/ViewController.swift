637d363c92735693ec8c8703dd5af7ec0b220d56
Ayman-Naim <mnameno_2012@yahoo.com>
2024-01-03 21:10:14 +0200

    Initial Commit

---
+++ Movie-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-App
+//
+//  Created by ayman on 03/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
